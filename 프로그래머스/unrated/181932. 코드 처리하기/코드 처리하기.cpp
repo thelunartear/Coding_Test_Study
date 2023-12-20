2abc2c1711d88fd2eac31ac45b5c2d0d4159756c
thelunartear <108967089+thelunartear@users.noreply.github.com>
2023-12-21 01:00:34 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    int idx = code.length();
+    int mode = 0;
+    
+    for(int i=0; i<idx; i++)
+    {
+        if(mode==0)
+        {
+            if(code[i]=='1')
+                mode=1;
+            else
+            {
+                if(i%2==0)
+                    answer+=code[i];
+            }
+        }
+        else
+        {
+            if(code[i]=='1')
+                mode=0;
+            else
+            {
+                if(i%2==1)
+                    answer+=code[i];
+            }
+        }
+    }
+    if(answer== "")
+        answer="EMPTY";
+    return answer;
+}
