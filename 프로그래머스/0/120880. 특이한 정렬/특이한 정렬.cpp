787a45583d8c52257de291ac152bf8393e2c308d
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-03-03 02:52:36 +0900

    [level 0] Title: 특이한 정렬, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> numlist, int n) {
+    sort(numlist.begin(), numlist.end(), [n](int a, int b)
+         {
+            if (abs(a - n) == abs(b - n)) {
+            return a > b;
+        }
+        return abs(a - n) < abs(b - n);
+    });
+    return numlist;
+}
