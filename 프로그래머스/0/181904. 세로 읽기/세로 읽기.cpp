89f52787f7c72f9d57addc41e5b50399d86b6215
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-03-06 02:09:26 +0900

    [level 0] Title: 세로 읽기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
@@ -5,17 +5,7 @@ using namespace std;
 
 string solution(string my_string, int m, int c) {
     string answer = "";
-    vector<string> div;
-    for (int i = 0; i < my_string.size(); i += m) 
-    {
-        div.push_back(my_string.substr(i, m));
-    }
-    
-    for (const string& s : div) {
-        if (c - 1 < s.size()) 
-        {
-            answer += s[c - 1];
-        }
-    }
+    for(int i=c-1; i<my_string.length(); i+=m)
+        answer += my_string[i];
     return answer;
 }
