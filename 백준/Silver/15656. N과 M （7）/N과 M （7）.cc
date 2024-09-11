12d29541f189c81ee0daf16a1d736d16008b91fd
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-09-12 03:01:18 +0900

    [Silver III] Title: N과 M (7), Time: 408 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[9];
+int num[9];
+
+void func(int k)
+{
+    if(k==m)
+    {
+        for(int i=0;i<m;i++)
+        {
+             cout<<num[arr[i]]<<' ';
+        }
+        cout<<"\n";
+        return;
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+       arr[k]=i;
+       func(k+1);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>num[i];
+    }
+    sort(num, num+n);
+    func(0);
+}
