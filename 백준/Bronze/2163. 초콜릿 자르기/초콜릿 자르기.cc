f0542e8a2e55c8f0ed7744339dfcdbc8f0743e45
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-09-18 18:50:55 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    int n, m;
+    cin>>n>>m;
+    cout<<n*m-1;
+}
