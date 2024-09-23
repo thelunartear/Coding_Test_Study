ee625f77c0ba0e4500d287f6057d4eda7c0e2992
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-09-24 01:22:17 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+long long factorial(int n)
+{
+    if(n==0 || n==1)
+        return 1;
+    else
+        return n*factorial(n-1);
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin>>n;
+    cout<<factorial(n);
+}
