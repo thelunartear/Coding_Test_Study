5fbac27555e91d5b14d010f466842fac89570f62
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-10-14 22:19:39 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, ans=0;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    sort(arr, arr+n);
+
+    for(int i=0;i<n;i++)
+    {
+        ans += arr[i]*(n-i);
+    }
+
+    cout<<ans;
+}
