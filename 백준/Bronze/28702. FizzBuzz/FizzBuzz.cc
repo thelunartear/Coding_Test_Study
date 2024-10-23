2bb0227a7731ca2a4300430059daadced07d6cc7
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-10-23 13:34:37 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -1,43 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int getFizzBuzzNumber(string res, int start) {
-    if (res == "FizzBuzz") {
-        return (start + 14) / 15 * 15;
-    } else if (res == "Fizz") {
-        for (int i = start;; i++) {
-            if (i % 3 == 0 && i % 5 != 0) return i;
-        }
-    } else if (res == "Buzz") {
-        for (int i = start;; i++) {
-            if (i % 5 == 0 && i % 3 != 0) return i;
-        }
-    } else {
-        int num = stoi(res);
-        return num;
-    }
-}
+string s[3];
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    string str1, str2, str3;
-    cin >> str1 >> str2 >> str3;
+    for(int i=0;i<3;i++)
+    {
+        cin>>s[i];
+    }   
 
-    int start = 1;
-    start = getFizzBuzzNumber(str1, start) + 1;
-    start = getFizzBuzzNumber(str2, start) + 1;
-    start = getFizzBuzzNumber(str3, start) + 1;
+    for(int i=0;i<3;i++)
+    {
+        if(s[i][0]=='F' || s[i][0]=='B')
+        {
+            continue;
+        }
+        
+        int ans = stoi(s[i])+3-i;
 
-    if (start % 3 == 0 && start % 5 == 0)
-        cout << "FizzBuzz\n";
-    else if (start % 3 == 0)
-        cout << "Fizz\n";
-    else if (start % 5 == 0)
-        cout << "Buzz\n";
-    else
-        cout << start << "\n";
-
-    return 0;
+        if(ans%3 && ans%5)
+            cout<<ans;
+        else
+        {
+            if(ans%3==0)
+                cout<<"Fizz";
+            if(ans%5==0)
+                cout<<"Buzz";
+        }
+        break;
+    }    
 }
