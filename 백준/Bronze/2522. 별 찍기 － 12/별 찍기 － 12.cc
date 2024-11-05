92de0c1939144e572a1ae64f1186732d9807aac1
thelunartear <108967089+thelunartear@users.noreply.github.com>
2024-11-05 11:13:32 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+char arr[200][101];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        for (int j=0;j<n-i-1;j++) 
+            cout << ' ';
+    
+        for (int j=0;j<=i;j++) 
+            cout << '*'; 
+    
+        cout << "\n";
+    }
+
+    for(int i=n;i<2*n-1;i++)
+    {
+        for(int j=0;j<i-n+1;j++)
+            cout<<' ';
+        for(int j=0;j<2*n-i-1;j++)
+            cout<<'*';
+        cout<<"\n";
+    }
+}
