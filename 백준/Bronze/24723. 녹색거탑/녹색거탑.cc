9469f22c3626fc6365cab2f66b2c1a7f05386d88
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-02-20 09:38:55 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+
+    cout<<pow(2,n);
+}
