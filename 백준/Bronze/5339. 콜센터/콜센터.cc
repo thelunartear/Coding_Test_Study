5fc31ce5c07a91c45e53583b134d712600cd89d8
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-03-07 23:47:36 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout<<"     /~\\\n";
+    cout<<"    ( oo|\n";
+    cout<<"    _\\=/_\n";
+    cout<<"   /  _  \\\n";
+    cout<<"  //|/.\\|\\\\\n";
+    cout<<" ||  \\ /  ||\n";
+    cout<<"============\n";
+    cout<<"|          |\n";
+    cout<<"|          |\n";
+    cout<<"|          |\n";
+}
