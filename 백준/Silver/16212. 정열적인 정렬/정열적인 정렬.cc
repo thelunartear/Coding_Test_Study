8af365467df4a35a16ee43b6c172f6d2cfb5e331
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-03-26 19:08:43 +0900

    [Silver V] Title: 정열적인 정렬, Time: 0 ms, Memory: 2024 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a[1001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+
+    if(n<=1000)
+    {
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        sort(a,a+n);
+        for(int i=0;i<n;i++)
+            cout<<a[i]<<' ';
+    }
+}
