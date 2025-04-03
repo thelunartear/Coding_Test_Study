0d8a083c6d0eb56518eb1b0ffa7ac7119ca9fa1e
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-04-03 21:49:00 +0900

    [Bronze IV] Title: 과목선택, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11948. 과목선택/과목선택.cc
+++ 백준/Bronze/11948. 과목선택/과목선택.cc
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> v1;
-vector<int> v2;
+int a[4];
+int b[2];
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int a,b,c,d,e,f;
-    cin>>a>>b>>c>>d>>e>>f;
+    for(int i=0;i<4;i++)
+    {
+        cin>>a[i];
+    }
 
-    v1.push_back(a);
-    v1.push_back(b);
-    v1.push_back(c);
-    v1.push_back(d);
-    v2.push_back(e);
-    v2.push_back(f);
+    for(int i=0;i<2;i++)
+    {
+        cin>>b[i];
+    }
 
-    sort(v1.begin(),v1.end());
-    sort(v2.begin(),v2.end());
+    sort(a,a+4);
+    sort(b,b+2);
 
-    cout<<v1[1]+v1[2]+v1[3]+v2[1];
+    cout<<a[1]+a[2]+a[3]+b[1];
 }
