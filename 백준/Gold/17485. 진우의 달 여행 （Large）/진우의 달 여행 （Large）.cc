325f437e5d769184d53b66c02bf87065bd2ad2f1
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-04-17 23:44:32 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 84 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[1001][1001];
+int dp[1001][1001][3];
+int dy[]={-1,0,1};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,m;
+    cin>>n>>m;
+
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            cin>>a[i][j];
+
+    for(int j=0;j<m;j++)
+        for(int k=0;k<3;k++)
+            dp[0][j][k]=a[0][j];
+
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            for(int k=0;k<3;k++)
+            {
+                int p=INT_MAX;
+                for(int d=0;d<3;d++)
+                {
+                    if(d!=k)
+                    {
+                        int py=j+dy[d];
+                        if(py>=0&&py<m)
+                            p=min(p,dp[i-1][py][d]);
+                    }
+                }
+                dp[i][j][k]=a[i][j]+p;
+            }
+        }
+    }
+
+    int ans=INT_MAX;
+    for(int j=0;j<m;j++)
+        for(int k=0;k<3;k++)
+            ans=min(ans,dp[n-1][j][k]);
+
+    cout<<ans;
+}
