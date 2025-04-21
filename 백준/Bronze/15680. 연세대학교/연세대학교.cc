347b20e3898b8e6a8a7883c0cf9a26df41e9444f
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-04-21 17:42:27 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    if(n==0)
+        cout<<"YONSEI";
+    else
+        cout<<"Leading the Way to the Future";
+}
