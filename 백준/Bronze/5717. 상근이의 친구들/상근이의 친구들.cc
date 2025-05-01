86dee1d9d5de284f5424b373a6dd26ba1603ccad
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-05-01 17:02:08 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    while(true)
+    {
+        int a,b;
+        cin>>a>>b;
+
+        if(a!=0&&b!=0)
+            cout<<a+b<<"\n";
+        else
+            break;
+    }
+}
