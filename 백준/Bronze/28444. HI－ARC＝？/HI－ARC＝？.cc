a8243a6aa54b021ff14efc9846f5f446c6bc7ff3
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-05-04 22:29:46 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int h,i,a,r,c;
+    cin>>h>>i>>a>>r>>c;
+
+    cout<<h*i-a*r*c;
+}
