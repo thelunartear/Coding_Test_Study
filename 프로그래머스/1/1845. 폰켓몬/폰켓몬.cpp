c8640d13fb18f6ae30b49ed20ef06808c90fcc92
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-09-18 01:44:36 +0900

    [level 1] Title: 폰켓몬, Time: 0.75 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int answer = 0;
+    int a=nums.size()/2;
+    sort(nums.begin(),nums.end());
+    nums.erase(unique(nums.begin(),nums.end()),nums.end());
+    int b=nums.size();
+    answer=min(a,b);
+    return answer;
+}
