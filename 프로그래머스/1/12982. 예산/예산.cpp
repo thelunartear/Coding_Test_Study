e14a80bd56132b2355c51d01ec13cd35f523b06f
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-09-19 15:49:29 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.cpp
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -8,12 +8,18 @@ using namespace std;
 int solution(vector<int> d, int budget) {
     int answer = 0;
     sort(d.begin(),d.end());
-    auto it=d.begin();
-    while(it!=d.end()&&budget>=*it)
+    int sum=0;
+    for(int i=0;i<d.size();i++)
     {
-        budget-=*it;
-        answer++;
-        ++it;
+        if(sum+d[i]<=budget)
+        {
+            sum+=d[i];
+            answer++;
+        }
+        else
+        {
+            break;
+        }
     }
     return answer;
 }
