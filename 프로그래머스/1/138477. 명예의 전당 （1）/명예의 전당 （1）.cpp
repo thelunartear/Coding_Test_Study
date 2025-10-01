924d832df790816feafeaec4036c279a2ec04eb2
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-10-01 20:54:59 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.12 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    vector<int> temp;
+    for(int i=0;i<score.size();i++)
+    {
+        temp.push_back(score[i]);
+        sort(temp.begin(), temp.end(), greater<int>());
+
+        if(temp.size()>k)
+        {
+            temp.pop_back();
+        }
+        answer.push_back(temp.back());
+    }
+    return answer;
+}
