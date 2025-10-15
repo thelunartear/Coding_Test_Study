269f6c78f025bb730cbd7a8446440d657534b712
thelunartear <108967089+thelunartear@users.noreply.github.com>
2025-10-16 00:44:40 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+
+    int idx1=0,idx2=0;
+    for(string g:goal)
+    {
+        if(idx1<cards1.size()&&g==cards1[idx1])
+        {
+            idx1++;
+        }
+        
+        else if(idx2<cards2.size()&&g==cards2[idx2])
+        {
+            idx2++;
+        }
+        else
+        {
+            return "No";
+        }
+    }
+    
+    
+    return "Yes";
+}
